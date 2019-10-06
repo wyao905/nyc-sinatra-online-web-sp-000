3794d2aa03a40ad0e0612144c07d421d472e25ba
Warren Yao <warrenyca905@gmail.com>
2019-10-06 22:36:58 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,41 @@
 class FiguresController < ApplicationController
   # add controller methods
+    get '/figures' do
+    @figures = Figure.all
+    erb :"/figures/index"
+  end
+  
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :"/figures/new"
+  end
+  
+  post '/figures' do
+    figure = Figure.create(params[:figure])
+    figure.landmarks << Landmark.create(params[:landmark])
+    figure.titles << Title.create(params[:title])
+    redirect "/figures"
+  end
+  
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+  
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/edit'
+  end
+  
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.name = params[:figure][:name]
+    @figure.landmarks << Landmark.create(params[:landmark])
+    @figure.titles << Title.create(params[:title])
+    @figure.save
+    redirect "/figures/#{params[:id]}"
+  end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,16 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :"/landmarks/index"
+  end
+  
+  get'/landmarks/new' do
+    @titles = Title.all
+    @figures = Figure.all
+    erb :"/landmarks/new"
+  end
+  
+  post '/landmarks' do
+  end
 end
